86053b205aee8cd549c4bb34128a95274c4df147
Mohammad Ali Hadikhah <hadikhahe@gmailcom>
2023-10-27 15:13:02 +0000

    fix package installer script

--- server_package_install.sh
+++ server_package_install.sh
@@ -3,3 +3,9 @@ echo type the packages you want to install :
 read packages
 
 docker compose exec nodejs_server npm i $packages
+
+docker compose stop nodejs_server
+
+docker compose build nodejs_server
+
+docker compose start nodejs_server
