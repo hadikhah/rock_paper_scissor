aaa4c3deb62422278e42084e1bdbca6262c0b28a
Mohammad Ali Hadikhah <hadikhahe@gmailcom>
2023-10-28 11:31:58 +0000

    add checkUnique to validator middleware

--- server/src/middlewares/validator.ts
+++ server/src/middlewares/validator.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from "express"
 import Ajv from "ajv"
 import addFormats from "ajv-formats"
+import { Model } from "mongoose"
+import { ValidationError, catchAsyncError } from "./Exeption"
 
 // TODO add ajv error messages package
 // TODO check more on types to be more specific
@@ -41,11 +43,60 @@ const validate = (schema: Object, req: Request, res: Response, next: NextFunctio
  */
 const validator = (schema: Object) => {
 
-    return (req: Request, res: Response, next: NextFunction) => {
+    return catchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
 
-        validate(schema, req, res, next)
+        await validate(schema, req, res, next)
 
+    })
+
+}
+
+
+/**
+*
+*
+* @param {Model<Schema>} model
+* @param {String} field
+* @param {*} data
+* @return {*} 
+*/
+export const checkUnique = (model: Model<any>, field: any,) => {
+
+    return catchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+
+        await isUnique(model, field, req.body[field], req, res, next)
+
+    })
+}
+
+/**
+ *
+ *
+ * @param {Model<Schema>} model
+ * @param {String} field
+ * @param {*} data
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+const isUnique: any = async (model: Model<Object>, field: any, data: any, req: Request, res: Response, next: NextFunction) => {
+
+    console.log(field, data)
+    const isUnique = ! await model.findOne({ [field]: data }).lean()
+
+    const errorMessage = `${field} is already taken`
+
+    const ErrorObject = {
+        instancePath: field,
+        message: errorMessage
     }
+
+    console.log(isUnique)
+
+    if (!isUnique)
+        throw new ValidationError(errorMessage, 422, [ErrorObject])
+
+    next()
 
 }
 
