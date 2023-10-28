0875304c7f106b670171f7f8a7675add71dd6e04
Mohammad Ali Hadikhah <hadikhahe@gmailcom>
2023-10-28 21:05:51 +0000

    add cookies and cors to config

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -2,6 +2,8 @@ import express, { Express } from "express"
 import morgan from "morgan"
 import { existsSync, writeFileSync } from "fs";
 import path from "path";
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
 import connect from "./db"
 import { LoggerStream } from "./winston"
@@ -14,6 +16,17 @@ const config = async (app: Express) => {
     const dbAddress = process.env.MONGO_URI || "http://localhost:27017/example_db"
 
     connect(dbAddress)
+
+    // cors
+    app.use(
+        cors({
+            origin: ["http://localhost:5000", process.env.CLIENT_URL || "http://localhost:5000", process.env.SERVER_URL || "http://localhost:3000"],
+            credentials: true
+        })
+    )
+
+    // cookies
+    app.use(cookieParser())
 
     // encode url and buffers
     app.use(express.urlencoded({ extended: false }))
