5842aeeccdcbcf68779fccb01fdb0593ee09e60a
Mohammad Ali Hadikhah <hadikhahe@gmailcom>
2023-11-09 12:07:19 +0000

    install nodeMailer and ejs and add mail utils

---
+++ server/src/utils/mail.ts
@@ -0,0 +1,96 @@
+import { TransportOptions, createTransport } from "nodemailer"
+import ejs from "ejs"
+import path from "path";
+import { catchAsyncError } from "../middlewares/Exception";
+
+interface TransportMailOptionsCredentials {
+    user?: String
+    pass?: String
+}
+
+interface TransportMailOptions extends TransportOptions {
+    host?: String
+    port?: number | String
+    secure?: Boolean | null
+    auth?: TransportMailOptionsCredentials
+}
+
+/**
+ * email schema options
+ * 
+ *  @type {*} 
+*/
+const mailOptions: TransportMailOptions = {
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
+    secure: process.env.MAIL_SECURE == "true" ? true : (process.env.MAIL_SECURE == "false") ? false : null,
+    auth: {
+        user: process.env.MAIL_USERNAME,
+        pass: process.env.MAIL_PASSWORD,
+    },
+}
+
+/** 
+ *  email schema
+ * @type {*}
+ */
+const transporter = createTransport(mailOptions);
+
+/**
+ * send email using html body format
+ *
+ * @param {string} receivers
+ * @param {string} subject
+ * @param {string} htmlBody
+ * @return {*} 
+ */
+export const sendMailHTML = async (receivers: string, subject: string, htmlBody: string) => {
+
+    const info = await transporter.sendMail({
+        from: process.env.MAIL_FROM, // sender address
+        to: receivers, // list of receivers
+        subject: subject, // Subject line
+        html: htmlBody, // html body
+    });
+
+    return info
+
+}
+
+/**
+ * send email using text only format
+ *
+ * @param {string} receivers
+ * @param {string} subject
+ * @param {string} textBody
+ * @return {*} 
+ */
+export const sendMailTEXT = async (receivers: string, subject: string, textBody: string) => {
+
+    const info = await transporter.sendMail({
+        from: process.env.MAIL_FROM, // sender address
+        to: receivers, // list of receivers
+        subject: subject, // Subject line
+        text: textBody, // plain text body
+    });
+
+    return info
+
+}
+
+/**
+ * renders ejs file with given data and returns html string
+ *
+ * @param {string} template
+ * @param {*} [data={}]
+ * @return {*} 
+ */
+export const renderTemplateEjs = async (template: string, data = {}) => {
+
+    const templatePath = path.join(__dirname, `../templates/${template}`)
+
+    const html = await ejs.renderFile(templatePath, data)
+
+    return html
+
+}
