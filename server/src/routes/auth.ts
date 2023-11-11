5cb753fb29bef993b7a4baa93a8de2b5a92130af
Mohammad Ali Hadikhah <hadikhahe@gmailcom>
2023-11-11 18:32:03 +0000

    add send and check email verification code routes

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,9 +1,13 @@
 import { Request, Response, Router } from "express";
 
 import User from "../models/User";
+
 import validator, { checkUnique } from "../middlewares/validator";
-import { signUpFieldsValidationSchema, signup } from "../controllers/auth/SignUpController";
+import { protect } from "../middlewares/auth";
+
+import { resendVerificationCodeEmail, signUpFieldsValidationSchema, signup } from "../controllers/auth/SignUpController";
 import { signIn, signInFieldsValidationSchema } from "../controllers/auth/SignInController";
+import { checkVerificationCodeEmail, checkVerificationCodeFieldsValidationSchema } from "../controllers/auth/EmailVerificationController";
 
 const router = Router();
 
@@ -16,5 +20,9 @@ router.get("/", (req: Request, res: Response) => {
 router.post("/signup", validator(signUpFieldsValidationSchema), checkUnique(User, "email"), signup);
 
 router.post("/signin", validator(signInFieldsValidationSchema), signIn)
+
+router.post("/resend-email-verification-code", protect, resendVerificationCodeEmail)
+
+router.post("/check-email-verification-code", validator(checkVerificationCodeFieldsValidationSchema), protect, checkVerificationCodeEmail)
 
 export default router
